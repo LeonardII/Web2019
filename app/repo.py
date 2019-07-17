7c56e24f548beb2d849d9982ed845d65893a7b2a
Leonard Faix <leonard.faix@googlemail.com>
2019-07-17 14:49:40 +0200

    bhbhjlbjh

--- app/repo.py
+++ app/repo.py
@@ -1,3 +1,4 @@
+import random
 from dataModels import *
 from run import db
 
@@ -23,7 +24,20 @@ def insertQuestion(frage, optionen, antwort):
         db.session.add(q)
         db.session.commit()
 
+def getRandomQuestion():
+    rand = random.randrange(0, session.query(Question).count())
+    row = session.query(Question)[rand]
+    return row
+        
+
 def addUser(name, email, passwort):
     u = User(Name=name, Email=email, Passwort=passwort)
     db.session.add(u)
     db.session.commit()
+
+def loginUser(email, passwort):
+    q = session.query(User).filter(User.Email == email, User.Passwort == passwort)
+    exists = session.query(q.exists())
+    db.session.commit()
+    return exists
+
--- app/run.py
+++ app/run.py
@@ -11,6 +11,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://GzkqBfvtZN:nfhSObaxFY@r
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 from dataModels import *
 
@@ -65,10 +67,34 @@ def register():
         return redirect(url_for('start'))
     return render_template('register.html', form=form)  
 
-@app.route('/login')
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not is_safe_url(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('play'))
     return render_template('login.html', form=form)  
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route('/submit')
 def submit():
